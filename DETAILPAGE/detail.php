bd95b5a4e6ebace59c70e45482a03c262b0901ae
turu0d <tsuru321aq@icloud.com>
2020-11-20 10:17:09 +0900

    不要な部分の削除と更新

--- DETAILPAGE/detail.php
+++ DETAILPAGE/detail.php
@@ -11,63 +11,48 @@ try{
 	$stmt = $dbh->prepare($sql);
 	$stmt->execute();
 	
-	$dbh = null;
-	
-	while(true){
-		$rec = $stmt->fetch(PDO::FETCH_ASSOC);
-		if($rec==false){
-			break;
-		}
-		print $rec['ID'];
-		print $rec['name'];
-		print $rec['TEL'];
-		print $rec['address'];
-		print $rec['URL'];
-		print $rec['reserve_site_URL'];
-		print $rec['image'];
-		print $rec['site_value'];
-		print $rec['sns_value'];
-		print $rec['characteristic'];
+	while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+		$data[] = $row;
 	}
 } catch (PDOException $e){
-	print 'DB接続エラー';
+	echo($e->getMessage());
 	die();
 }
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>温泉詳細画面</title>
-        <link href="css/reset.css" rel="stylesheet" type="text/css">
-        <link href="css/css.css" rel="stylesheet" type="text/css">
-    </head>
+  <head>
+    <meta charset="UTF-8">
+		<title>温泉詳細画面</title>
+		<link href="css/reset.css" rel="stylesheet" type="text/css">
+		<link href="css/css.css" rel="stylesheet" type="text/css">
+  </head>
     
-    <body>
-        <header>
-            <h1><?php echo $rec['name'].温泉; ?></h1>
-            <a href="#"><?php echo $rec['TEL']; ?></a>
-            <br>住所<?php echo $rec['address']; ?>
-        </header>
-        <div id="branding">
-        </div>
-        <nav>
-            <ul>
-                <li><a href="#">口コミサイトの口コミ抜粋(まとめ)・評価(**/5.0)・数値・(グラフ)・(キーワード)</a></li>
-                <li><a href="#">SNSの口コミ抜粋（まとめ）・評価（**/5.0）・数値・（グラフ）・（キーワード）</a></li>
-            </ul>
-        </nav>
-        <section>
-            <article>
-                <span class="date"></span>
-                <h2></h2>
-                <p>
-                    
-                </p>
-                <a href="#" class="btn"></a>
-            </article>
-        </section>
-        <footer>
-            <small></small>
-        </footer>
+  <body>
+		<header>
+			<?php echo '<h1>'.$row['name'].'温泉</h1>'; ?>
+			<div style="text-align: right;">
+				<a href="#"><?php echo $row['TEL']; ?></a>
+				<p>住所：<?php echo $row['address']; ?></p>
+			</div>
+		</header>
+			<div id="branding">
+			</div>
+				<nav>
+					<ul>
+						<li><a href="#">口コミサイトの口コミ抜粋(まとめ)・評価(**/5.0)・数値・(グラフ)・(キーワード)</a></li>
+						<li><a href="#">SNSの口コミ抜粋（まとめ）・評価（**/5.0）・数値・（グラフ）・（キーワード）</a></li>
+          </ul>
+				</nav>
+		<section>
+			<article>
+				<span class="date"></span>
+					<h2></h2>
+					<p></p>
+					<a href="#" class="btn"></a>
+      </article>
+    </section>
+			<footer>
+				<small></small>
+      </footer>
     </body>
 </html>
