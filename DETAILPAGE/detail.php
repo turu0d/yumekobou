ab2eb1dfbbf8e7d3d9b3cd6a72fbb37d2e5c3ed5
turu0d <tsuru321aq@icloud.com>
2020-11-23 15:06:11 +0900

    レーダーチャート追加

--- DETAILPAGE/detail.php
+++ DETAILPAGE/detail.php
@@ -1,5 +1,4 @@
 <?php
-	
 	$dsn = 'mysql:host=database-2.cnjcx8ih0byc.ap-northeast-1.rds.amazonaws.com;dbname=onsen_db;charset=utf8';
 	$user = 'admin';
 	$pass = 'rootroot1';
@@ -23,6 +22,7 @@ try{
   <head>
     <meta charset="UTF-8">
 		<title>温泉詳細画面</title>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script> <!-- ChartJSのインポート -->
   </head>
     
   <body>
@@ -39,8 +39,16 @@ try{
 			</div>
 				<nav>
 					<ul>
-						<li>口コミサイトの口コミ抜粋(まとめ)・評価(**/5.0)・数値・(グラフ)・(キーワード)</li>
-						<li>SNSの口コミ抜粋（まとめ）・評価（**/5.0）・数値・（グラフ）・（キーワード）</li>
+						<li> <!-- SNS評価のグラフ -->
+								<div style="height:350px; width:500px; margin:0 auto;">   <!-- グラフ表示枠を規定 -->
+    							<canvas id="SNSChart"></canvas>   <!-- ここにグラフを表示 -->
+  							</div>
+						</li>
+						<li> <!-- 口コミ評価のグラフ -->
+								<div style="height:350px; width:500px; margin:0 auto;">   <!-- グラフ表示枠を規定 -->
+    							<canvas id="KuchikomiChart"></canvas>   <!-- ここにグラフを表示 -->
+  							</div>
+						</li>
           </ul>
 				</nav>
 		<section>
@@ -54,5 +62,107 @@ try{
 			<footer>
 				<small></small>
       </footer>
+<script>
+var ctx = document.getElementById("SNSChart");
+var myRadarChart = new Chart(ctx, {
+  //グラフの種類
+  type: 'radar',
+  //データの設定
+  data: {
+      //データ項目のラベル
+      labels: ["満足度", "価格", "心地よさ", "泉質", "料理"],
+      //データセット
+      datasets: [
+          {
+              label: "SNS評価",
+              //背景色
+              backgroundColor: "rgba(200,112,126,0.5)",
+              //枠線の色
+              borderColor: "rgba(200,112,126,1)",
+              //結合点の背景色
+              pointBackgroundColor: "rgba(200,112,126,1)",
+              //結合点の枠線の色
+              pointBorderColor: "#fff",
+              //結合点の背景色（ホバ時）
+              pointHoverBackgroundColor: "#fff",
+              //結合点の枠線の色（ホバー時）
+              pointHoverBorderColor: "rgba(200,112,126,1)",
+              //結合点より外でマウスホバーを認識する範囲（ピクセル単位）
+              hitRadius: 5,
+              //グラフのデータ
+              data: [4,3,4,4,4]
+          }
+      ]
+  },
+ options: {
+    // レスポンシブ指定
+    responsive: true,
+    scale: {
+      ticks: {
+        // 最小値の値を0指定
+        beginAtZero:true,
+        min: 0,
+        // 最大値を指定
+        max: 5,
+      }
+    },
+		title: {
+			display: true, //タイトルの表示可否
+			text: 'AIを用いたSNS評価解析結果' //タイトル
+		}
+  }
+});
+</script>
+<script>
+var ctx = document.getElementById("KuchikomiChart");
+var myRadarChart = new Chart(ctx, {
+  //グラフの種類
+  type: 'radar',
+  //データの設定
+  data: {
+      //データ項目のラベル
+      labels: ["満足度", "価格", "心地よさ", "泉質", "料理"],
+      //データセット
+      datasets: [
+          {
+              label: "SNS評価",
+              //背景色
+              backgroundColor: "rgba(0,0,255,0.5)",
+              //枠線の色
+              borderColor: "rgba(0,0,255,1)",
+              //結合点の背景色
+              pointBackgroundColor: "rgba(0,0,255,1)",
+              //結合点の枠線の色
+              pointBorderColor: "#fff",
+              //結合点の背景色（ホバ時）
+              pointHoverBackgroundColor: "#fff",
+              //結合点の枠線の色（ホバー時）
+              pointHoverBorderColor: "rgba(200,112,126,1)",
+              //結合点より外でマウスホバーを認識する範囲（ピクセル単位）
+              hitRadius: 5,
+              //グラフのデータ
+              data: [5,2,3,1,6]
+          }
+      ]
+  },
+ options: {
+    // レスポンシブ指定
+    responsive: true,
+    scale: {
+      ticks: {
+        // 最小値の値を0指定
+        beginAtZero:true,
+        min: 0,
+        // 最大値を指定
+        max: 5,
+      }
+    },
+		title: {
+			display: true, //タイトルの表示可否
+			text: 'AIを用いた口コミ評価解析結果' //タイトル
+		}
+  }
+});
+</script>
     </body>
 </html>
